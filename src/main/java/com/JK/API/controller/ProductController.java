b9c4d9ec45aacfc3752f9689b59b4cc23da3b598
JulianKleist <julianlopez.j.l.52@gmail.com>
2021-05-25 19:01:00 -0400

    API

---
+++ src/main/java/com/JK/API/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.JK.API.controller;
+
+import com.JK.API.entity.Product;
+import com.JK.API.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products){
+        return service.saveProducts(products);
+    }
+
+    @GetMapping("/products")
+    public List<Product> findAllProducts(){
+        return service.getProducts();
+    }
+
+    @GetMapping("/productId/{id}")
+    public Product findProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/product/{name}")
+    public Product findProductByName(@PathVariable String name){
+        return service.getProductByName(name);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product){
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable int id){
+        return service.deleteProduct(id);
+    }
+}
---
+++ src/main/java/com/JK/API/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.JK.API.repository;
+
+import com.JK.API.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findByName(String name);
+}
